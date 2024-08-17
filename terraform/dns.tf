1f291fc49210d7a4b0e9b69e1a0f492e7c87e2c6
Marcelo Soffredi <msoffredi@gmail.com>
2024-08-17 18:34:55 -0500

    Adjusted project in DNS resource for the staging environment

--- terraform/dns.tf
+++ terraform/dns.tf
@@ -13,6 +13,7 @@ data "google_dns_managed_zone" "domain_zone" {
 
 # Add the IP to the DNS
 resource "google_dns_record_set" "website" {
+    project      = var.domain_project_id != null ? var.domain_project_id : var.project_id
     provider     = google
     name         = "webapp${var.deploy_env == "prod" ? "" : "-"}${var.deploy_prefix}.${data.google_dns_managed_zone.domain_zone.dns_name}"
     type         = "A"
