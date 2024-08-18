c54e4115517dfe6d38e76a3f9910bdd4fd789b20
Marcelo Soffredi <msoffredi@gmail.com>
2024-08-17 20:39:50 -0500

    Fixed production domain

--- terraform/dns.tf
+++ terraform/dns.tf
@@ -15,7 +15,7 @@ data "google_dns_managed_zone" "domain_zone" {
 resource "google_dns_record_set" "website" {
     project      = var.domain_project_id != null ? var.domain_project_id : var.project_id
     provider     = google
-    name         = "webapp${var.deploy_env == "prod" ? "" : "-"}${var.deploy_prefix}.${data.google_dns_managed_zone.domain_zone.dns_name}"
+    name         = "webapp${var.deploy_env == "prod" ? "" : "-"}${var.deploy_env == "prod" ? "" : var.deploy_prefix}.${data.google_dns_managed_zone.domain_zone.dns_name}"
     type         = "A"
     ttl          = 300
     managed_zone = data.google_dns_managed_zone.domain_zone.name
